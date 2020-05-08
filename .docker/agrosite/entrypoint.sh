bc4c6cc700d38f91607f4605fd05b0cc41d6823a
webdevych <lee.darren1218@gmail.com>
2020-05-08 13:53:30 +0530

    production commit

--- .docker/agrosite/entrypoint.sh
+++ .docker/agrosite/entrypoint.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 
 /wait-for-it.sh $REDIS_HOST:$REDIS_PORT -t 60
 
-cd /opt/agro_api
+cd /opt/agrosite
 
 # install pip env deps, run migrations, collect media, start the server
 pip install -r requirements.txt --quiet
--- agrosite/wsgi.py
+++ agrosite/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agro_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agrosite.settings")
 
 from django.core.wsgi import get_wsgi_application
 
