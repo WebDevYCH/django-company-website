d43c701f79ff252d29a6595fd0732fd2c8428e23
webdevych <lee.smith870327@gmail.com>
2020-08-02 20:57:17 +0530

    blog ui commit

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,17 +2,6 @@
 # encoding: utf-8
 
 
-"""
-@version: ??
-@author: liangliangyy
-@license: MIT Licence
-@contact: liangliangyy@gmail.com
-@site: https://www.lylinux.net/
-@software: PyCharm
-@file: urls.py
-@time: 2016/11/2 下午7:15
-"""
-
 from django.urls import path
 from django.views.decorators.cache import cache_page
 from . import views
